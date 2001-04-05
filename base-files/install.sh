00c950bfc37aecf681d485ed4f48700a75e3dd9e
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-04-05 13:33:59 +0000

    New directory with base files independent of the actual package manager.

---
+++ base-files/install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -e
+#
+# install.sh - install fink-base package
+#
+# Fink - a package manager that downloads source and installs it
+# Copyright (c) 2001 Christoph Pfisterer
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ./install.sh <prefix>"
+  echo "  Example: ./install.sh /sw"
+  exit 1
+fi
+
+basepath=$1
+
+
+echo "Creating directories..."
+
+mkdir -p $basepath
+chmod 755 $basepath
+
+for dir in etc bin sbin lib libexec include \
+	   share share/info share/man share/doc \
+	   info man \
+	   var var/run var/spool src ; do
+  mkdir $basepath/$dir
+  chmod 755 $basepath/$dir
+done
+
+
+echo "Copying files..."
+
+cp init.sh $basepath/bin/
+chmod 644 $basepath/bin/init.sh
+cp init.csh $basepath/bin/
+chmod 644 $basepath/bin/init.csh
+
+cp fink-release $basepath/etc/
+chmod 644 $basepath/etc/fink-release
+
+
+echo "Done."
+exit 0
