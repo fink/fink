f371ecca8dbee6185e42d9597ced4cb56dc3dbac
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-07-15 20:04:05 +0000

    Add dir-base to the base-files tarball in bootstrap.pl.

--- bootstrap.pl
+++ bootstrap.pl
@@ -335,7 +335,7 @@ $script .=
 $script .=
   "cd base-files && ".
   "tar -cf $installto/src/base-files-$packageversion.tar ".
-  "fink-release init.csh.in init.sh.in install.sh setup.sh\n";
+  "fink-release init.csh.in init.sh.in dir-base install.sh setup.sh\n";
 
 foreach $cmd (split(/\n/,$script)) {
   next unless $cmd;   # skip empty lines
