ba0bed50aa6825083c15e572704eee7dc9732451
Max Horn <fingolfin@users.sourceforge.net>
2001-12-27 22:54:23 +0000

    ups, typo

--- perlmod/Fink/Config.pm
+++ perlmod/Fink/Config.pm
@@ -4,7 +4,7 @@
 # Fink - a package manager that downloads source and installs it
 # Copyright (c) 2001 Christoph Pfisterer
 #
-# This proram is free software; you can redistribute it and/or
+# This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
