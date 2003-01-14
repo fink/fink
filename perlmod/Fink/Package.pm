43cb8656a8539927aa8cd8168e088f46edb3fde8
Finlay Dobbie <finlayd@users.sourceforge.net>
2003-01-14 14:50:47 +0000

    Adding back a minor fix

--- perlmod/Fink/Package.pm
+++ perlmod/Fink/Package.pm
@@ -112,7 +112,8 @@ sub add_version {
   my $version_object = shift;
 
   my $version = $version_object->get_fullversion();
-  if (exists $self->{_versions}->{$version}) {
+  if (exists $self->{_versions}->{ $version} 
+    && $self->{_versions}->{ $version}->{_type} eq 'dummy' ) {
     $self->{_versions}->{$version}->merge($version_object);
   } else {
     $self->{_versions}->{$version} = $version_object;
