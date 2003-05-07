4164fdf7e9b440702a92494a55260cb50a0985f4
Ben Hines <benh57@users.sourceforge.net>
2003-05-07 00:46:27 +0000

    support 10.2.6

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -151,10 +151,10 @@ sub check_host {
 	} elsif ($host =~ /^powerpc-apple-darwin5\.[0-5]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.1";
-	} elsif ($host =~ /^powerpc-apple-darwin6\.[0-5]/) {
+	} elsif ($host =~ /^powerpc-apple-darwin6\.[0-6]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.2";
-	} elsif ($host =~ /^powerpc-apple-darwin(6\.[6-9]|[7-9]\.)/) {
+	} elsif ($host =~ /^powerpc-apple-darwin(6\.[7-9]|[7-9]\.)/) {
 		&print_breaking("This system was not released at the time " .
 			"this Fink release was made, but should work.");
 		$distribution = "10.2";
