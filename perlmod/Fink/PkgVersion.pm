f1440b2bd028f19d1657e43ac94d7e021bd808af
Justin F. Hallett <thesin@users.sourceforge.net>
2003-08-28 00:51:09 +0000

    Fix my first commit and added NoPerlTests to validate, as a correct field

--- perlmod/Fink/PkgVersion.pm
+++ perlmod/Fink/PkgVersion.pm
@@ -1279,7 +1279,7 @@ sub phase_compile {
 		$compile_script =
 			"perl$perlversion Makefile.PL PERL=perl$perlversion PREFIX=\%p INSTALLPRIVLIB=\%p/lib/perl5$perldirectory INSTALLARCHLIB=\%p/lib/perl5$perldirectory/darwin INSTALLSITELIB=\%p/lib/perl5$perldirectory INSTALLSITEARCH=\%p/lib/perl5$perldirectory/darwin INSTALLMAN1DIR=\%p/share/man/man1 INSTALLMAN3DIR=\%p/share/man/man3 INSTALLSITEMAN1DIR=\%p/share/man/man1 INSTALLSITEMAN3DIR=\%p/share/man/man3 INSTALLBIN=\%p/bin INSTALLSITEBIN=\%p/bin INSTALLSCRIPT=\%p/bin\n".
 			"make\n";
-			unless ($self->has_param("NoPerlTests")) {
+			unless ($self->param_boolean("NoPerlTests")) {
 				$compile_script .= "make test";
 			}
 	} else {
