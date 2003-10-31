74669cf156e95f2e44ff606ff91518f322785c07
Michael G Schwern <schwern@users.sourceforge.net>
2003-10-31 02:07:19 +0000

    Making search_comparedb() honor symlinked .info files like it used to.

--- perlmod/Fink/Package.pm
+++ perlmod/Fink/Package.pm
@@ -398,7 +398,7 @@ sub search_comparedb {
         # Using find is much faster than doing it in Perl
         return
           (grep !m{/(CVS|binary-$debarch)/},
-           `find $path -type f -name '*.info' -newer $basepath/var/db/fink.db`)
+           `find $path \\( -type f -or -type l \\) -and -name '*.info' -newer $basepath/var/db/fink.db`)
              ? 1 : 0;
 }
 
