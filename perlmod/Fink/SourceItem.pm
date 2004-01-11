b57e91ac4586fea41c91a217488911bd40f0b902
Daniel Macks <dmacks@users.sourceforge.net>
2004-01-11 22:05:59 +0000

    Just a skeleton for future work.

---
+++ perlmod/Fink/SourceItem.pm
@@ -0,0 +1,43 @@
+#
+# Fink::SourceItem class
+#
+# Fink - a package manager that downloads source and installs it
+# Copyright (c) 2001 Christoph Pfisterer
+# Copyright (c) 2001-2004 The Fink Package Manager Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA	 02111-1307, USA.
+#
+
+package Fink::SourceItem;
+use Fink::Config qw($basepath);
+use Fink::NetAccess qw(&fetch_url_to_file);
+use Fink::Services qw(&expand_percent &filename);
+
+use strict;
+use warnings;
+
+BEGIN {
+	use Exporter ();
+	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+	$VERSION	 = 1.00;
+	@ISA		 = qw(Exporter);
+	@EXPORT		 = qw();
+	@EXPORT_OK	 = qw();
+	%EXPORT_TAGS = ( );
+}
+
+END { }
+
+1;
