80377e95d26e5187b4ef7a1bd5e962833374b74b
David R. Morrison <dmrrsn@users.sourceforge.net>
2004-03-17 01:34:26 +0000

    change from pathsetup.command to pathsetup.sh

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -282,7 +282,7 @@ sub fink_packagefiles {
 
 my $packagefiles = "COPYING INSTALL INSTALL.html README README.html USAGE USAGE.html Makefile ".
   "ChangeLog VERSION fink.in fink.8.in fink.conf.5.in install.sh setup.sh ".
-  "shlibs.default.in pathsetup.command.in postinstall.pl.in perlmod update t ".
+  "shlibs.default.in pathsetup.sh.in postinstall.pl.in perlmod update t ".
   "fink-virtual-pkgs.in";
 
 return $packagefiles;
