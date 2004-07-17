29c5eabbcefd2b972338df14d90e3ce9b31bc738
David R. Morrison <dmrrsn@users.sourceforge.net>
2004-07-17 01:18:35 +0000

    sync with HEAD

--- mirror/inject.pl
+++ mirror/inject.pl
@@ -45,7 +45,7 @@ foreach $file (qw(ChangeLog _keys _list)) {
 }
 
 my $packagefiles = "COPYING ChangeLog README install.sh postinstall.pl.in " .
-    "_keys _list apt cpan ctan cvs-repository debian gimp gnome gnu kde master rsync sourceforge website" ;
+    "_keys _list _urls apt cpan ctan cvs-repository debian gimp gnome gnu kde master rsync sourceforge website" ;
 
 my $info_script = "";
 
