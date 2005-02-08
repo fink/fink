8e759145572ba1c75c15a52c67c752db9aa662dc
Daniel Macks <dmacks@users.sourceforge.net>
2005-02-08 13:36:56 +0000

    Oops.

--- perlmod/Fink/CLI.pm
+++ perlmod/Fink/CLI.pm
@@ -39,9 +39,9 @@ BEGIN {
 	# your exported package globals go here,
 	# as well as any optionally exported functions
 	@EXPORT_OK	 = qw(&print_breaking &print_breaking_stderr
-					  &prompt &prompt_boolean &prompt_selection &prompt_selection
+					  &prompt &prompt_boolean &prompt_selection
 					  &print_optionlist
-			      &get_term_width);
+					  &get_term_width);
 }
 our @EXPORT_OK;
 
