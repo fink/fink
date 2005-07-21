27341c0faf3499207abb6cdb1a1699b5d81b9764
David R. Morrison <dmrrsn@users.sourceforge.net>
2005-07-21 17:08:15 +0000

    just in case we ever make another release on this branch

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -177,7 +177,7 @@ $gcc = Fink::Services::enforce_gcc("Under CURRENT_SYSTEM, Fink must be bootstrap
 		&print_breaking("This system was not released at the time " .
 			"this Fink release was made, but should work.");
 		$distribution = "10.3";
-	} elsif ($host =~ /^powerpc-apple-darwin8\.[0-1]\.0/) {
+	} elsif ($host =~ /^powerpc-apple-darwin8\.[0-2]\.0/) {
 		&print_breaking("This system is supported and tested.");
 		if($ENV{FINK_NOTRANS}) {
 			&print_breaking("Using the non-transitional tree...");
