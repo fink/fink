0e6d44965d19ff7999e97a3334d3545ec4340dbe
Alexander Hansen <alexkhansen@users.sourceforge.net>
2005-11-16 17:23:05 +0000

    Support 10.4.3?  Brilliant!

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -175,7 +175,7 @@ GCC_MSG
 		&print_breaking("This system was not released at the time " .
 			"this Fink release was made, but should work.");
 		$distribution = "10.3";
-	} elsif ($host =~ /^powerpc-apple-darwin8\.[0-2]\.0/) {
+	} elsif ($host =~ /^powerpc-apple-darwin8\.[0-3]\.0/) {
 		&print_breaking("This system is supported and tested.");
 		if($ENV{FINK_NOTRANS}) {
 			&print_breaking("Using the non-transitional tree...");
@@ -194,7 +194,7 @@ GCC_MSG
 		} else {
 			$distribution = "10.4-transitional";
 		}
-	} elsif ($host =~ /^i386-apple-darwin8\.[0-2]\.[0-1]/) {
+	} elsif ($host =~ /^i386-apple-darwin8\.[0-3]\.[0-1]/) {
 		&print_breaking("Fink is currently not supported on x86 ".
 			"Darwin. Various parts of Fink hardcode 'powerpc' ".
 			"and assume to run on a PowerPC based operating ".
