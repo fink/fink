409151c50c250e4ed4dca514ad01375aac088d8c
David R. Morrison <dmrrsn@users.sourceforge.net>
2007-01-22 18:48:24 +0000

    Happy New Year

--- perlmod/Fink/Checksum/SHA256.pm
+++ perlmod/Fink/Checksum/SHA256.pm
@@ -3,7 +3,7 @@
 # Fink::Checksum::SHA256 module
 #
 # Fink - a package manager that downloads source and installs it
-# Copyright (c) 2005-2006 The Fink Package Manager Team
+# Copyright (c) 2005-2007 The Fink Package Manager Team
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
