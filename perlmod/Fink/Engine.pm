7dc8daacb38b5c5a3d72135180cabce2c461675b
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-03-19 16:58:01 +0000

    revert OldIndexes stuff

--- perlmod/Fink/Engine.pm
+++ perlmod/Fink/Engine.pm
@@ -1607,31 +1607,6 @@ sub real_install {
 		{
 			warn "unable to open $basepath/fink/dists: $!";
 		}
-
-
-		if (not -f $cache_file or (-M $cache_file > 14)) {
-			my $oldindexes = lc(Fink::Config::get_option("OldIndexes", "warn"));
-			if ($oldindexes !~ /^(ignore|update|warn)$/) {
-				$oldindexes = 'warn';
-				print_breaking_stderr "WARNING: unknown value for 'OldIndexes' in fink.conf: $oldindexes";
-			}
-	
-			my $up_to_date_text;
-			if (-f $cache_file)
-			{
-				$up_to_date_text = "WARNING: your info file index has not been updated for " . int(-M $cache_file) . " days.";
-			} else {
-				$up_to_date_text = "WARNING: your info file index does not exist.";
-			}
-	
-			if ($oldindexes eq "warn") {
-				print_breaking_stderr $up_to_date_text . "  You should run 'fink selfupdate' to get the latest package descriptions.\n";
-			} elsif ($oldindexes eq "update") {
-				print_breaking_stderr $up_to_date_text . "  Fink will now update it automatically.";
-				require Fink::SelfUpdate;
-				Fink::SelfUpdate::check();
-			}
-		}
 	}
 
 	# add requested packages
