a949d354904dad4d9efc84bc9f957b0347e925df
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-10-27 17:08:59 +0000

    backport gcc_select fix

--- perlmod/Fink/Services.pm
+++ perlmod/Fink/Services.pm
@@ -1279,9 +1279,11 @@ selection cannot be determined.
 =cut
 
 sub gcc_selected {
-	if (-x '/usr/sbin/gcc_select') {
-		chomp(my $gcc_select = `/usr/sbin/gcc_select 2>&1`);
-		return $gcc_select if $gcc_select =~ s/^.*gcc version (\S+)\s+.*$/$1/gs;
+	if (not get_osx_vers() ge "10.5") {
+		if (-x '/usr/sbin/gcc_select') {
+			chomp(my $gcc_select = `/usr/sbin/gcc_select 2>&1`);
+			return $gcc_select if $gcc_select =~ s/^.*gcc version (\S+)\s+.*$/$1/gs;
+		}
 	}
 	if (-l '/usr/bin/gcc') {
 		my $link = readlink('/usr/bin/gcc');
@@ -1302,6 +1304,7 @@ breakage. This function checks for such breakage and fixes it if necessary.
 =cut
 
 sub fix_gcc_repairperms {
+	return if (get_osx_vers() ge "10.5");
 	return unless gcc_select_arg(gcc_selected) eq '4.0';
 	if (-x '/usr/sbin/gcc_select') {
 		system('/usr/bin/env PATH=/usr/sbin:/usr/bin:/sbin:/bin '
