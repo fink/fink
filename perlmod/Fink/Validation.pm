c604da54635d9cd9fbf3b3ddb45cf316db8c5551
Daniel Macks <dmacks@users.sourceforge.net>
2008-05-14 05:53:02 +0000

    Private shared-libraries are not intended to be stable or sane from the outside: no reason to require their less-than-install_name-named fils to be in a separate -dev pkg. Also tweak a msg wording.

--- perlmod/Fink/Validation.pm
+++ perlmod/Fink/Validation.pm
@@ -1640,8 +1640,8 @@ sub _validate_dpkg {
 				push(@installed_dylibs, $filename);
 			}
 			my( $fn_name, $fn_ext ) = $filename =~ /^(.*)(\..*)/g;  # parse apart at extension
-			if ( (grep /^\Q$fn_name\E.+\Q$fn_ext\E$/, sort keys %$deb_shlibs) && !(exists $deb_shlibs->{$filename})) {
-				&stack_msg($msgs, "Files less specifically versioned than a Shlibs entry do not belong in this package", $filename);
+			if ( (grep { /^\Q$fn_name\E.+\Q$fn_ext\E$/ && !$deb_shlibs->{$_}->{is_private} } sort keys %$deb_shlibs) && !(exists $deb_shlibs->{$filename})) {
+				&stack_msg($msgs, "Files with names less specifically versioned than ones in public Shlibs entries do not belong in this package", $filename);
 			}
 		}
 
