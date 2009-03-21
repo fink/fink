f6c4ed3f917201849e769294afc36288fc2d9231
David R. Morrison <dmrrsn@users.sourceforge.net>
2009-03-21 14:47:27 +0000

    make sure $osMajorVer(sion) has been defined

--- fink.csh
+++ fink.csh
@@ -1,4 +1,6 @@
 # this script should only be installed when Architecture is i386
+set osMajorVersion = `uname -r | cut -d. -f1`
+
 if ( $osMajorVersion > 9 ) then
   setenv VERSIONER_PERL_PREFER_32_BIT yes
 endif
--- fink.sh
+++ fink.sh
@@ -1,4 +1,6 @@
 # this script should only be installed when Architecture is i386
+osMajorVer=`uname -r | cut -d. -f1`
+
 if [ $osMajorVer -gt 9 ]; then
   VERSIONER_PERL_PREFER_32_BIT=yes
   export VERSIONER_PERL_PREFER_32_BIT
