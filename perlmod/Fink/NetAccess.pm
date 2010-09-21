c182f8d9d6cc4f695eedcf382553dbc32b7b517a
Alexander Hansen <alexkhansen@users.sourceforge.net>
2010-09-21 18:07:47 +0000

    backport from HEAD

--- perlmod/Fink/NetAccess.pm
+++ perlmod/Fink/NetAccess.pm
@@ -397,9 +397,10 @@ sub download_cmd {
 			if ($config->param_boolean("ProxyPassiveFTP")) {
 				$cmd .= " --passive-ftp";
 			}
-			if ($file ne &filename($url)) {
+			#if ($file ne &filename($url)) {
+			# always use -O to handle complex URLs
 				$cmd .= " -O $cmd_file";
-			}
+			#}
 			if ($cont) {
 				$cmd .= " -c"
 			}
