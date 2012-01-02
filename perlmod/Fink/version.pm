441d69bfcc14ac7eb1f13e10cf4a2ae3b86930a2
Alexander Hansen <alexanderk.hansen@gmail.com>
2012-01-01 19:47:34 -0500

    Happy New Year

--- perlmod/Fink/version.pm
+++ perlmod/Fink/version.pm
@@ -5,7 +5,7 @@
 # Copyright (C) 2004,2005,2006,2007 John Peacock
 #
 # For the changes by Fink:
-# Copyright (C) 2007-2011 The Fink Package Manager Team.
+# Copyright (C) 2007-2012 The Fink Package Manager Team.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
