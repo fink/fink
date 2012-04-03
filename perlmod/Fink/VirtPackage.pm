47eba924f7521e19ff94b02674eee6fa6e2940c6
Alexander Hansen <alexanderk.hansen@gmail.com>
2012-04-03 14:39:37 -0700

    Use more robust version regexes

--- perlmod/Fink/VirtPackage.pm
+++ perlmod/Fink/VirtPackage.pm
@@ -931,6 +931,7 @@ the successful execution of "gcc --version".
 
 The clang virtual package is considered present based on
 the successful execution of "/usr/bin/clang --version".
+the successful execution of "/usr/bin/clang -v".
 
 =cut
     
@@ -944,6 +945,8 @@ the successful execution of "/usr/bin/clang --version".
 				close(CLANG);
 				if ($versionoutput =~ m|Apple\sclang\sversion\s(\d+.\d+)\s\(tags/Apple/clang\-(\d+\.\d+\.\d+)|) {
 					($version, $build) = ($1, $2);
+				if ($versionoutput =~ m|Apple\sclang\sversion\s(\d+.\d+(\.\d+)?)\s\(tags/Apple/clang\-(\d+.\d+(\.\d+)?)|) {
+					($version, $build)= ($1, $3);
 				} else {
 					print STDERR "  - warning, unable to determine the version for clang\n" if ($options{debug});
 				}
