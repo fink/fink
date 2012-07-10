e9fb80fe16007f6f7b182053f146dcfbe4020d9a
justin <jhallett@goarctic.com>
2012-07-09 18:53:19 -0600

    Fix .la files before md5sums is built

--- perlmod/Fink/PkgVersion.pm
+++ perlmod/Fink/PkgVersion.pm
@@ -4422,14 +4422,42 @@ EOF
 		preprocess => sub {
 			# Don't descend into the .deb control directory
 			return () if $File::Find::dir eq "$destdir/DEBIAN";
-			# only return .la files
-			return () if ( $_ ~= /\.la$/);
 			return @_;
 
 		},
 		wanted => sub {
-			if (-f $_ && ! -l $_) {
-				#perl -pi -e "s/^(dependency_libs)=.*/\1=''/" "$_" || true
+			if (-f $_ && ! -l $_ && $_ =~ m/\.la$/) {
+				print "Reading ".$_.".\n";
+				my $filechanged = 0;
+				if ( open(LA,"<$File::Find::name") ) {
+					my @lalines;
+					while (my $laline = <LA>) {
+						if ($laline =~ m/^dependency_libs=/) {
+							print "Clearing dependency_lib from ".$_.".\n";
+							$laline =~ s/^(dependency_libs)=.*/$1=''/;
+							$filechanged = 1;
+						}
+						push(@lalines, $laline);
+					}
+					close(LA) or die "can't clear dependency_libs of ".$_.": $!\n";
+					if ($filechanged) {
+						print "Writing new ".$_.".\n";
+						if ( open(NEWLA,">$File::Find::name") ) {
+							foreach my $newlaline (@lalines) {
+								print NEWLA $newlaline;
+							}
+							close(NEWLA) or die "can't clear dependency_libs of ".$_.": $!\n";
+						} else {
+							my $error = "can't clear dependency_libs of ".$_.": $!";
+							$notifier->notify(event => 'finkPackageBuildFailed', description => $error);
+							die $error . "\n";
+						}
+					}
+				} else {
+					my $error = "can't clear dependency_libs of ".$_.": $!";
+					$notifier->notify(event => 'finkPackageBuildFailed', description => $error);
+					die $error . "\n";
+				}
 			}
 		},
 	}, $destdir
