690de53edfd74b283d199406603fcb813534a1bd
Alexander Hansen <alexanderk.hansen@gmail.com>
2012-07-10 16:55:39 -0700

    Hardcode Xcode's "cvs".

--- perlmod/Fink/SelfUpdate/CVS.pm
+++ perlmod/Fink/SelfUpdate/CVS.pm
@@ -37,6 +37,7 @@ use File::Find;
 use strict;
 use warnings;
 
+my $cvs="/usr/bin/cvs"; # Only one provider as of 10.5.
 our $VERSION = 1.00;
 
 =head1 NAME
@@ -74,8 +75,8 @@ sub system_check {
 		}
 	}
 
-	if (not Fink::VirtPackage->query_package("dev-tools")) {
-		warn "Before changing your selfupdate method to 'cvs', you must install ".
+	unless ((-x $cvs) and Fink::VirtPackage->query_package("dev-tools")) {
+		warn "Before changing your selfupdate method to 'cvs', you must install".
 		     $line2.
 		     "http://connect.apple.com (after free registration)".
 		     $line4.".\n";
@@ -236,7 +237,7 @@ sub setup_direct_cvs {
 						"for a password, just press return (i.e. the password ".
 						"is empty).");
 		if ($cvsrepository =~ s/^:local://)  {
-			$cmd = "cvs ${verbosity} -z3 -d$cvsrepository";
+			$cmd = "$cvs ${verbosity} -z3 -d$cvsrepository";
  		}
  		else {
 			$cmd = qq(cvs -d":pserver${proxcmd}:anonymous\@$cvsrepository" login);
@@ -369,7 +370,7 @@ sub do_direct_cvs {
 
 	@sb = stat("$descdir/CVS");
 
-	$cmd = "cvs ${verbosity} -z3 update -d -P -l";
+	$cmd = "$cvs ${verbosity} -z3 update -d -P -l";
 
 	$msg = "I will now run the cvs command to retrieve the latest package descriptions. ";
 
@@ -402,7 +403,7 @@ sub do_direct_cvs {
 
 	my @trees = split(/\s+/, $config->param_default("SelfUpdateTrees", $config->param_default("SelfUpdateCVSTrees", $distribution)));
 	for my $tree (@trees) {
-		$cmd = "cvs ${verbosity} -z3 update -d -P ${tree}";
+		$cmd = "$cvs ${verbosity} -z3 update -d -P ${tree}";
 		$cmd = "/usr/bin/su $username -c '$cmd'" if ($username);
 		if (&execute($cmd)) {
 			$errors++;
