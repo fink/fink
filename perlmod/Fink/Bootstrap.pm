9fbb86255a12ce59a3849b6eea8c5cca190f2006
Alexander Hansen <alexanderk.hansen@gmail.com>
2012-09-19 21:00:24 -0700

    I'm showing kernel 11.4.2 on my 10.7 box.

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -208,7 +208,7 @@ GCC_MSG
 			"of Mac OS X might work with Fink, but there are no " .
 			"guarantees.");
 		$distribution = "10.6";
-	} elsif ($host =~ /^i386-apple-darwin11\.[0-5]\.0/) {
+	} elsif ($host =~ /^i386-apple-darwin11\.[0-4]\.[0-2]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.7";
 	} elsif ($host =~ /^i386-apple-darwin11\./) {
