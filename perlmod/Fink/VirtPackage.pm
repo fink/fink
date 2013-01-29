254d95ece836ee67001e5a0ab5912a9fe6e1e364
Alexander Hansen <alexanderk.hansen@gmail.com>
2013-01-28 18:16:39 -0700

    Fix clang detection on Xcode 4.6+. Thanks to danielj.

--- perlmod/Fink/VirtPackage.pm
+++ perlmod/Fink/VirtPackage.pm
@@ -1027,8 +1027,8 @@ the successful execution of "/usr/bin/clang -v".
 				my ($versionoutput, $version, $build);
 				{ local $/ = undef; $versionoutput = <CLANG> }
 				close(CLANG);
-				if ($versionoutput =~ m|Apple\sclang\sversion\s(\d(?:\.\d+(?:\.\d+)?)?)\s\(tags/Apple/clang\-(\d+(?:\.\d+(?:\.\d+)?)?)|) {
-					($version, $build)= ($1, $2);
+				if ($versionoutput =~ m[Apple\s(clang|LLVM)\sversion\s(\d(?:\.\d+(?:\.\d+)?)?)\s\((tags/Apple/)?clang\-(\d+(?:\.\d+(?:\.\d+)?)?)]) {
+					($version, $build)= ($2, $4);
 				} else {
 					print STDERR "  - warning, unable to determine the version for clang\n" if ($options{debug});
 				}
