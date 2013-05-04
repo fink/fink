6328b7943bb81735adbf504e6f10c2960fbb036f
Matthias Neeracher <neeracher@mac.com>
2013-05-04 23:57:12 +0200

    Extrapolate distribution names for future releases

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -226,6 +226,12 @@ GCC_MSG
 			"of Mac OS X might work with Fink, but there are no " .
 			"guarantees.");
 		$distribution = "10.8";
+	} elsif ($host =~ /^i386-apple-darwin(\d+)\./) {
+		&print_breaking("This system was not released at the time " .
+			"this Fink release was made.  Prerelease versions " .
+			"of Mac OS X might work with Fink, but there are no " .
+			"guarantees.");
+		$distribution = "10." . ($1-4);
 	} else {
 		&print_breaking("This system is unrecognized and not ".
 			"supported by Fink.");
