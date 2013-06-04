7231d958c1695a97469e5d193c556d1b66655bc6
Alexander Hansen <alexanderk.hansen@gmail.com>
2013-06-04 16:48:08 -0700

    Support 10.8.4

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -217,7 +217,7 @@ GCC_MSG
 			"of Mac OS X might work with Fink, but there are no " .
 			"guarantees.");
 		$distribution = "10.7";
-	} elsif ($host =~ /^i386-apple-darwin12\.[0-3]\.[0-1]/) {
+	} elsif ($host =~ /^i386-apple-darwin12\.[0-4]\.[0-1]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.8";
 	} elsif ($host =~ /^i386-apple-darwin12\./) {
