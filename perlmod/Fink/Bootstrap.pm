ecdc6ffeb935f9b3780043fbeb9e0b31c493f9a4
Alexander Hansen <alexkhansen@users.sourceforge.net>
2015-07-10 15:50:19 -0700

    Officially support 10.10.4

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -219,7 +219,7 @@ GCC_MSG
 			"of Mac OS X might work with Fink, but there are no " .
 			 "guarantees.");
 		$distribution = "10.9";
-	} elsif ($host =~ /^i386-apple-darwin14\.[0-3]/) {
+	} elsif ($host =~ /^i386-apple-darwin14\.[0-4]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.10";
 	} elsif ($host =~ /^i386-apple-darwin14\./) {
