c08fcad1844f0d6ee8fc31d1af59af641f69c21b
Daniel Johnson <daniel@daniel-johnson.org>
2017-02-10 20:27:20 +0100

    Add selfupdate-svn command.

--- perlmod/Fink/Engine.pm
+++ perlmod/Fink/Engine.pm
@@ -119,6 +119,7 @@ our %commands =
 	  'self-update'       => [\&cmd_selfupdate,        0, 1, 1],
 	  'selfupdate-cvs'    => [\&cmd_selfupdate_cvs,    0, 1, 1],
 	  'selfupdate-rsync'  => [\&cmd_selfupdate_rsync,  0, 1, 1],
+	  'selfupdate-svn'    => [\&cmd_selfupdate_svn,    0, 1, 1],
 	  'selfupdate-finish' => [\&cmd_selfupdate_finish, 1, 1, 1],
 	  'validate'          => [\&cmd_validate,          0, 0, 0],
 	  'check'             => [\&cmd_validate,          0, 0, 0],
@@ -417,6 +418,10 @@ sub cmd_selfupdate_cvs {
 
 sub cmd_selfupdate_rsync {
 	&cmd_selfupdate('--method=rsync', @_);
+}
+
+sub cmd_selfupdate_svn {
+	&cmd_selfupdate('--method=svn', @_);
 }
 
 sub cmd_selfupdate_finish {
