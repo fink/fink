e26ea857f5dbe391aed446627323bfb0e57df7a2
Daniel Macks <dmacks@netspace.org>
2018-02-01 11:29:35 -0500

    Support OS X 10.13.3 and 10.13.4

--- perlmod/Fink/Bootstrap.pm
+++ perlmod/Fink/Bootstrap.pm
@@ -236,7 +236,7 @@ GCC_MSG
 			"of Mac OS X might work with Fink, but there are no " .
 			"guarantees.");
 		$distribution = "10.12";
-	} elsif ($host =~ /^i386-apple-darwin17\.[0-2]/) {
+	} elsif ($host =~ /^i386-apple-darwin17\.[0-4]/) {
 		&print_breaking("This system is supported and tested.");
 		$distribution = "10.13";
 	} elsif ($host =~ /^i386-apple-darwin17\./) {
