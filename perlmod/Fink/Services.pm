25d435874c3db6370bf155fa10838075fef7416f
Hanspeter Niederstrasser <nieder@users.sourceforge.net>
2023-09-22 06:30:17 -0500

    Services.pm::get_osx_vers() Apple again forgot to bump the darwin release with a minor version increase on Monterey and Ventura.

--- perlmod/Fink/Services.pm
+++ perlmod/Fink/Services.pm
@@ -1444,8 +1444,8 @@ sub get_osx_vers {
 	my $darwin_osx = get_darwin_equiv();
 	$sw_vers =~ s/^(\d+\.\d+).*$/$1/;
 	if ($sw_vers != $darwin_osx) {
-		if (($sw_vers == 11.6 && $darwin_osx == 11.5) || ($sw_vers == 11.7 && $darwin_osx == 11.5) || ($sw_vers == 12.6 && $darwin_osx == 12.5)) {
-			# special cases in Big Sur and Monterey where it's OK to have a mismatch
+		if (($sw_vers == 11.6 && $darwin_osx == 11.5) || ($sw_vers == 11.7 && $darwin_osx == 11.5) || ($sw_vers == 12.6 && $darwin_osx == 12.5) || ($sw_vers == 12.7 && $darwin_osx == 12.5) || ($sw_vers == 13.6 && $darwin_osx == 13.5)) {
+			# special cases in Big Sur, Monterey, and Ventura where it's OK to have a mismatch
 		} else {
 			die "$sw_vers does not match the expected value of $darwin_osx. Please run `fink selfupdate` to download a newer version of fink";
 		}
@@ -1530,10 +1530,12 @@ sub get_darwin_equiv {
 		return $darwin_osx{$kernel_vers} || '11.' . ($kernel_vers_minor-1);
 	} elsif ($kernel_vers == 21) {
 		# darwin21.1 == 12.0
-		# darwin21.6 == 12.5 or 12.6 handled in get_osx_vers()
+		# darwin21.2 == 12.1
+		# darwin21.6 == 12.5, 12.6, or 12.7 handled in get_osx_vers()
 		return $darwin_osx{$kernel_vers} || '12.' . ($kernel_vers_minor-1);
 	} elsif ($kernel_vers == 22) {
 		# darwin22.1 == 13.0
+		# darwin22.6 == 13.5 or 13.6 handled in get_osx_vers()
 		return $darwin_osx{$kernel_vers} || '13.' . ($kernel_vers_minor-1);
 	} elsif ($kernel_vers >= 23) {
 		# darwin23.0 == 14.0 (beta)
